8b99481473825da06b991c537b0f2c57b0f0f694
garylvov <67614381+garylvov@users.noreply.github.com>
2020-07-01 19:45:46 -0400

    Update getMyPosTest.py

--- getMyPosTest.py
+++ getMyPosTest.py
@@ -2,22 +2,23 @@ import rospy
 from turtlesim.msg import Pose
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
-
+#coords of turtle 1
 myX = 0.0
 myY = 0.0
 myTheta = 0.0
 
-
+#initializes node and subscribes 
 def nodes():
         rospy.init_node('readPOS', anonymous=True)
         getSelfPose = rospy.Subscriber('turtle1/pose', Pose, getMyPose)
         
+#Callback funciton to update coords         
 def getMyPose(myPose):
     global myX, myY, myTheta
     myX = myPose.x
     myY= myPose.y
     myTheta = myPose.theta
-
+#Prints and publishes coords to ensure it is working correctly
 def checkPose():
     pub = rospy.Publisher('myPOS', String, queue_size=10)
     rate = rospy.Rate(1) #1 hz
