fa3994be10da75dd5921826ada59b92709dcd4f6
garylvov <67614381+garylvov@users.noreply.github.com>
2020-07-01 20:15:22 -0400

    Update goToGoalTurtleV2.py

--- goToGoalTurtleV2.py
+++ goToGoalTurtleV2.py
@@ -9,7 +9,7 @@ import time
 import turtlesim.srv
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-
+#defines coords of goalTurtle and turtle1
 myX = 0.0
 myY = 0.0
 myTheta = 0.0
@@ -17,7 +17,7 @@ myTheta = 0.0
 goalX = 0.0
 goalY = 0.0
 goalTheta = 0.0
-
+#creates goalTurtle at random coords
 def createGoalTurtle():
     global goalX, goalY
     goalTurtle = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
@@ -26,43 +26,55 @@ def createGoalTurtle():
     goalTheta = round(random.uniform(0,6.3), 3) #6.3 is used as an approximation of 2pi
     goalTurtle(goalX, goalY, goalTheta, 'goalTurtle')
     time.sleep(.75)
-
+    
+#Removes goalTurtle 
 def removeTurtle():
     goalTurtle = rospy.ServiceProxy('kill', turtlesim.srv.Kill)
     goalTurtle('goalTurtle')
-
+    
+#Callback function to update coords of turtle1
 def getMyPose(myPos):
     global myX, myY, myTheta
     myX = myPos.x
     myY= myPos.y
     myTheta = myPos.theta
-
+    
+#Callback function to update coords of goalTurtle
 def getGoalPose(goalPos):
     global goalX, goalY, goalTheta
     goalX = goalPos.x
     goalY= goalPos.y
     goalTheta = goalPos.theta
-
+    
+#sets velocity of turtle1
 def velocity():
+    #sets variable type as velocity and sets to zero
     myVel = Twist()
     myVel.linear.x = 0
     myVel.angular.y = 0
     setVelocity.publish(myVel)
+    #infinite loop
     while not rospy.is_shutdown():
+        #x and y components of distance
         xAxis = goalX - myX
         yAxis = goalY - myY
+        #calulates distance and angle need to turn for turtle1
         distance = abs(((xAxis**2) + (yAxis**2))**.5)
         relativeAngle = (math.atan2(yAxis, xAxis)-myTheta)
+        #turtle1 turns to face goalTurtle first PID control
         if(-.05 > relativeAngle or relativeAngle > .05):
             myVel.linear.x = distance / 10
             myVel.angular.z = relativeAngle * 4
+            #Sets the rotation into the right direction
             if(abs(relativeAngle) > math.pi):
                 myVel.angular.z = (math.pi - relativeAngle) * 4
             setVelocity.publish(myVel)
+        #once properly turned goes to goal PID control
         if((-.05 < relativeAngle and relativeAngle < .05)):
             myVel.linear.x = distance * 3
             myVel.angular.z = 0
             setVelocity.publish(myVel)
+        #collision detection
         if(distance < .4):
             removeTurtle()
             createGoalTurtle()
@@ -71,6 +83,7 @@ def velocity():
 if __name__ == '__main__':
     try:
         createGoalTurtle()
+        #initiates node that subscribes to both turtle's position, and publishes to one turtles velocity
         rospy.init_node('goToGoalTurtleV2', anonymous=False)
         getSelfPose = rospy.Subscriber('turtle1/pose', Pose, getMyPose)
         goalPose = rospy.Subscriber('goalTurtle/pose', Pose, getGoalPose)
